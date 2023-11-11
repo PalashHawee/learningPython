074d839e9deb2f1227fc6813b6d30a19a823cc1e
Palash Hawee <haweepalash@gmail.com>
2023-11-11 21:37:13 +0530

    learnt error

---
+++ error handling/err1.py
@@ -0,0 +1,34 @@
+#try
+#except
+#finally
+
+#try block test a block of code
+#except handles error
+#finally lets you to execute code
+
+'''try:
+	print(x)
+except:
+	print('An exception has occured!')'''	
+
+#print(x)
+
+'''try:
+	print(x)
+except NameError:
+	print('Varibale X is not defined')
+except:
+	print('Something went wrong!')	'''
+
+
+try:
+	print('x')
+except:
+	print('something wrong')
+
+else:
+	print('nothing wrong')		
+
+
+
+
